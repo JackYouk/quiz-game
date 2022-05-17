d88453457ca0faeda888f882446f740a1ff0873a
Jack Youkstetter <jackyoukstetter1@gmail.com>
2022-05-16 21:39:36 -0700

    added psuedo code

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// generate start page -----------------------
+
+
+// initialize + generate 1 min counter ---------------------
+
+
+// generate quiz question page -----------------------
+// array of objects with quiz questions
+
+
+// generate endgame page --------------------------
+
+
+// generate score page --------------------------
+
+
+// 
