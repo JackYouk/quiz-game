965b97bfec509651ee20a84981d3b62431df35d5
Jack Youkstetter <jackyoukstetter1@gmail.com>
2022-05-18 17:02:53 -0700

    generated start page (no styling yet) using jquery, added an empty array of objects for the questions and answer, started to build loop to generate questions + answers onto the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,128 @@
-// generate start page -----------------------
+const root = $("#root");
+
+// generate start page ----------------------------------------------------------------------------------
+const title = $('<h1>')
+    .text("Codiene Quiz Game")
+    .addClass('title');
+
+const description = $('<p>')
+    .text("In this quiz, you will be answering a series of 10 multiple choice questions. Answer as many as possible in the one minute time limit.")
+    .addClass('description');
+
+const startButton = $('<button>')
+    .text("Start")
+    .addClass('startButton');
+
+const scoresButton = $('<button>')
+    .text("High Scores")
+    .addClass('scoresButton');
+
+// root.append(title);
+// root.append(description);
+// root.append(startButton);
+// root.append(scoresButton);
 
 
-// initialize + generate 1 min counter ---------------------
+// initialize + generate 1 min counter -------------------------------------------------------------------
 
 
-// generate quiz question page -----------------------
+// generate quiz question page ---------------------------------------------------------------------------
 // array of objects with quiz questions
+const questions = [
+    {
+        question: "bcwjbnewc",
+        answerA: "cwwc",
+        answerB: "cewewc",
+        answerC: "ecwew",
+        answerD: "wwe"
+    },
+    {
+        question: "",
+        answerA: "",
+        answerB: "",
+        answerC: "",
+        answerD: ""
+    },
+    {
+        question: "",
+        answerA: "",
+        answerB: "",
+        answerC: "",
+        answerD: ""
+    },
+    {
+        question: "",
+        answerA: "",
+        answerB: "",
+        answerC: "",
+        answerD: ""
+    },
+    {
+        question: "",
+        answerA: "",
+        answerB: "",
+        answerC: "",
+        answerD: ""
+    },
+    {
+        question: "",
+        answerA: "",
+        answerB: "",
+        answerC: "",
+        answerD: ""
+    },
+    {
+        question: "",
+        answerA: "",
+        answerB: "",
+        answerC: "",
+        answerD: ""
+    },
+    {
+        question: "",
+        answerA: "",
+        answerB: "",
+        answerC: "",
+        answerD: ""
+    },
+    {
+        question: "",
+        answerA: "",
+        answerB: "",
+        answerC: "",
+        answerD: ""
+    },
+    {
+        question: "",
+        answerA: "",
+        answerB: "",
+        answerC: "",
+        answerD: ""
+    },
+];
+
+for(let i = 0; i < questions.length; i++){
+    $('<h2>')
+        .text(questions[i].question)
+        .addClass('question' + i);
+    
+    $('<radio>')
+        .text(questions[i].answerA)
+        .addClass('answer' + i);
+    $('<radio>')
+        .text(questions[i].answerB)
+        .addClass('answer' + i);
+    $('<radio>')
+        .text(questions[i].answerC)
+        .addClass('answer' + i);
+    $('<radio>')
+        .text(questions[i].answerD)
+        .addClass('answer' + i);
+}
+root.append(root.children());
 
 
-// generate endgame page --------------------------
+// generate endgame page ---------------------------------------------------------------------------------
 
 
-// generate score page --------------------------
-
-
-// 
+// generate score page -----------------------------------------------------------------------------------
